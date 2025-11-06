6eb66a9cc02602532c29846e2dcf78812fbe84ce
Victor Gomes dos Santos <vgomes.sants@gmail.com>
2025-11-05 23:20:05 -0300

    Initial Configuration

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css';
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind funcionando! 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
