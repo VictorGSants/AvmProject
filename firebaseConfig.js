ebfcb1e14cfe1d7ac87a888d6990ef77f4977c4c
VictorGSants <victor.gomes0777@gmail.com>
2025-11-30 21:26:22 -0300

    fix: firebaseConfig using env variables

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,32 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// LÊ o ambiente (dev ou prod)
-const ENV = import.meta.env.VITE_ENV;
-
-// Configurações DEV vindas do .env
-const firebaseConfigDev = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MSG_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
-// Configurações PROD vindas da Vercel
-const firebaseConfigProd = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MSG_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
-};
-
-const config = ENV === "dev" ? firebaseConfigDev : firebaseConfigProd;
-
-const app = initializeApp(config);
+const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
